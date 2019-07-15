4b278cd93c2c2bc50683da70de054b892f5f7679
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:01:57 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/xml-lib "0.1.2"
+(defproject org.clojars.vladimirmarkovic86/xml-lib "0.1.3"
   :description "XML library"
   :url "http://github.com/VladimirMarkovic86/xml-lib"
   :license {:name "Eclipse Public License"
@@ -9,8 +9,5 @@
   :min-lein-version "2.0.0"
   
   :source-paths ["src/clj"]
-  :test-paths ["test/clj"]
-  
-  :jar-exclusions [#"README.md$"
-                   #"LICENSE$"])
+  :test-paths ["test/clj"])
 
